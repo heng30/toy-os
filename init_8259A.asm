8552aeb0949a99822d68fad421ed66a2450f8bc6
heng30 <2238288979@qq.com>
2024-12-07 15:42:45 +0800

    [*] update normally

--- init_8259A.asm
+++ init_8259A.asm
@@ -1,47 +1,47 @@
 init8259A:
-     mov  al, 011h
-     out  020h, al
-     call io_delay
+    mov  al, 011h
+    out  020h, al
+    call io_delay
 
-     out 0A0h, al
-     call io_delay
+    out 0A0h, al
+    call io_delay
 
-     mov al, 020h
-     out 021h, al
-     call io_delay
+    mov al, 020h
+    out 021h, al
+    call io_delay
 
-     mov  al, 028h
-     out  0A1h, al
-     call io_delay
+    mov  al, 028h
+    out  0A1h, al
+    call io_delay
 
-     mov  al, 004h
-     out  021h, al
-     call io_delay
+    mov  al, 004h
+    out  021h, al
+    call io_delay
 
-     mov  al, 002h
-     out  0A1h, al
-     call io_delay
+    mov  al, 002h
+    out  0A1h, al
+    call io_delay
 
-     mov  al, 001h
-     out  021h, al
-     call io_delay
+    mov  al, 001h
+    out  021h, al
+    call io_delay
 
-     out  0A1h, al
-     call io_delay
+    out  0A1h, al
+    call io_delay
 
-     mov  al, 11111001b ;允许键盘中断
-     out  021h, al
-     call io_delay
+    mov  al, 11111001b ;允许键盘中断
+    out  021h, al
+    call io_delay
 
-     mov  al, 11101111b ;允许鼠标中断
-     out  0A1h, al
-     call io_delay
+    mov  al, 11101111b ;允许鼠标中断
+    out  0A1h, al
+    call io_delay
 
-     ret
+    ret
 
 io_delay:
-     nop
-     nop
-     nop
-     nop
-     ret
+    nop
+    nop
+    nop
+    nop
+    ret
--- kernel/mouse_move.c
+++ kernel/mouse_move.c
@@ -16,11 +16,10 @@
 
 #define FLAGS_OVERRUN 0x0001
 
-#define MOUSE_PHASE_ERROR 0
-#define MOUSE_PHASE_UNINIT 1
-#define MOUSE_PHASE_ONE 2
-#define MOUSE_PHASE_TWO 3
-#define MOUSE_PHASE_THREE 4
+#define MOUSE_PHASE_UNINIT 0
+#define MOUSE_PHASE_ONE 1
+#define MOUSE_PHASE_TWO 2
+#define MOUSE_PHASE_THREE 3
 
 typedef struct {
     unsigned char *m_buf;
@@ -28,9 +27,10 @@ typedef struct {
 } fifo8_t;
 
 typedef struct {
-    unsigned char m_buf[3];
+    char m_buf[3];
     unsigned char m_phase;
-    int m_x, m_y, m_btn;
+    char m_rel_x, m_rel_y, m_btn;
+    int m_abs_x, m_abs_y;
 } mouse_dec_t;
 
 static unsigned char g_keybuf[64];
@@ -57,9 +57,11 @@ static fifo8_t g_mouseinfo = {
 static mouse_dec_t g_mdec = {
     .m_buf = {0, 0, 0},
     .m_phase = MOUSE_PHASE_UNINIT,
-    .m_x = 0,
-    .m_y = 0,
+    .m_rel_x = 0,
+    .m_rel_y = 0,
     .m_btn = 0,
+    .m_abs_x = 80,
+    .m_abs_y = 80,
 };
 
 int fifo8_put(fifo8_t *fifo, unsigned char data) {
@@ -118,31 +120,33 @@ void enable_mouse(void) {
 }
 
 void erase_mouse(unsigned char *vram) {
-    boxfill8(vram, g_xsize, COL8_008484, g_mx, g_my, g_mx + 15, g_my + 15);
+    boxfill8(vram, g_xsize, COL8_008484, g_mdec.m_abs_x, g_mdec.m_abs_y,
+             g_mdec.m_abs_x + 15, g_mdec.m_abs_y + 15);
 }
 
 void draw_mouse(unsigned char *vram) {
-    put_block(vram, g_xsize, 16, 16, g_mx, g_my, g_mcursor, 16);
+    put_block(vram, g_xsize, 16, 16, g_mdec.m_abs_x, g_mdec.m_abs_y, g_mcursor,
+              16);
 }
 
 void compute_mouse_position(void) {
-    g_mx += g_mdec.m_x;
-    g_my += g_mdec.m_y;
+    g_mdec.m_abs_x += g_mdec.m_rel_x;
+    g_mdec.m_abs_y += g_mdec.m_rel_y;
 
-    if (g_mx < 0) {
-        g_mx = 0;
+    if (g_mdec.m_abs_x < 0) {
+        g_mdec.m_abs_x = 0;
     }
 
-    if (g_my < 0) {
-        g_my = 0;
+    if (g_mdec.m_abs_y < 0) {
+        g_mdec.m_abs_y = 0;
     }
 
-    if (g_mx > g_xsize - 16) {
-        g_mx = g_xsize - 16;
+    if (g_mdec.m_abs_x > g_xsize - 16) {
+        g_mdec.m_abs_x = g_xsize - 16;
     }
 
-    if (g_my > g_ysize - 16) {
-        g_my = g_ysize - 16;
+    if (g_mdec.m_abs_y > g_ysize - 16) {
+        g_mdec.m_abs_y = g_ysize - 16;
     }
 }
 
@@ -152,6 +156,7 @@ void show_mouse_error(unsigned char data) {
     show_string(vram, g_xsize, 32, 64, COL8_FFFFFF, pstr);
 }
 
+// FIXME: 鼠标的数据可能并不是以3个为一组发送的
 int mouse_decode(unsigned char dat) {
     // 初始化鼠标成功后会收到一个`0xfa`
     if (g_mdec.m_phase == MOUSE_PHASE_UNINIT) {
@@ -179,18 +184,18 @@ int mouse_decode(unsigned char dat) {
         g_mdec.m_buf[2] = dat;
         g_mdec.m_phase = MOUSE_PHASE_ONE;
         g_mdec.m_btn = g_mdec.m_buf[0] & 0x07;
-        g_mdec.m_x = g_mdec.m_buf[1];
-        g_mdec.m_y = g_mdec.m_buf[2];
+        g_mdec.m_rel_x = g_mdec.m_buf[1];
+        g_mdec.m_rel_y = g_mdec.m_buf[2];
 
         if ((g_mdec.m_buf[0] & 0x10) != 0) {
-            g_mdec.m_x |= 0xffffff00;
+            g_mdec.m_rel_x |= 0xff00;
         }
 
         if ((g_mdec.m_buf[0] & 0x20) != 0) {
-            g_mdec.m_y |= 0xffffff00;
+            g_mdec.m_rel_y |= 0xff00;
         }
 
-        g_mdec.m_y = -g_mdec.m_y;
+        g_mdec.m_rel_y = -g_mdec.m_rel_y;
         return 1;
     }
 
@@ -205,6 +210,7 @@ void show_mouse_info(void) {
 
     show_debug_info(data);
 
+    // BUG: 解析逻辑有问题
     if (mouse_decode(data) != 0) {
         erase_mouse(vram);
         compute_mouse_position();
@@ -226,6 +232,7 @@ void show_keyboard_input(void) {
     show_pos += 32;
 }
 
+// BUG: 鼠标移动错误
 void mouse_move(void) {
     unsigned char *vram = g_boot_info.m_vga_ram;
     init_palette();
@@ -236,7 +243,8 @@ void mouse_move(void) {
     write_vga_desktop_background();
 
     // 显示鼠标
-    put_block(vram, g_xsize, 16, 16, g_mx, g_my, g_mcursor, 16);
+    put_block(vram, g_xsize, 16, 16, g_mdec.m_abs_x, g_mdec.m_abs_y, g_mcursor,
+              16);
 
     io_sti(); // 开中断
     enable_mouse();
