11c7d87fd48aa2c5ec04959ce30ffba9a6bab366
heng30 <2238288979@qq.com>
2024-12-11 02:05:31 +0800

    [*] update normally

--- kernel/win_sheet.c
+++ kernel/win_sheet.c
@@ -4,13 +4,18 @@
 #include "draw.h"
 #include "memory.h"
 
+// 不要修改这个值， 因为受到`map`类型为`unsigned char`限制
 #define MAX_SHEETS 256
+
 #define SHEET_UNUSE 0
 #define SHEET_USE 1
 
 // 窗口绘制层管理器
 typedef struct {
+    // 图层管理像素表，表中记录每个图层管理的像素。
+    // 在刷新图层时，只刷新自己管理的像素
     unsigned char *m_map;
+
     int m_top; // 图层数量
     win_sheet_t *m_sheets[MAX_SHEETS];
     win_sheet_t m_sheets0[MAX_SHEETS];
@@ -98,9 +103,11 @@ void win_sheet_refreshsub(int vx0, int vy0, int vx1, int vy1, int h0, int h1) {
                 if (vx0 <= vx && vx < vx1 && vy0 <= vy && vy < vy1) {
                     unsigned char c = buf[by * sht->m_bxsize + bx];
 
+                    // 判断是否是透明像素
                     if (c != sht->m_col_inv) {
                         int pos = vy * xsize + vx;
                         if (pos >= 0 && pos < max_pos) {
+                            // 判断像素是否归自己管
                             if (g_sheet_ctl->m_map[pos] == sid) {
                                 g_boot_info.m_vga_ram[pos] = c;
                             }
@@ -112,6 +119,7 @@ void win_sheet_refreshsub(int vx0, int vy0, int vx1, int vy1, int h0, int h1) {
     }
 }
 
+// 刷新当前图层，不会刷新其他图层
 void win_sheet_refresh(win_sheet_t *sht, int bx0, int by0, int bx1, int by1) {
     if (sht->m_height >= 0) {
         win_sheet_refreshsub(sht->m_vx0 + bx0, sht->m_vy0 + by0,
@@ -128,17 +136,22 @@ void win_sheet_slide(win_sheet_t *sht, int vx0, int vy0) {
     if (sht->m_height >= 0) {
         win_sheet_refreshmap(old_vx0, old_vy0, old_vx0 + sht->m_bxsize,
                              old_vy0 + sht->m_bysize, 0);
+
         win_sheet_refreshmap(vx0, vy0, vx0 + sht->m_bxsize, vy0 + sht->m_bysize,
                              sht->m_height);
 
+        // 刷新当前图层以下的图层
         win_sheet_refreshsub(old_vx0, old_vy0, old_vx0 + sht->m_bxsize,
                              old_vy0 + sht->m_bysize, BOTTOM_WIN_SHEET_HEIGHT,
                              sht->m_height - 1);
+
+        // 刷新当前图层
         win_sheet_refreshsub(vx0, vy0, vx0 + sht->m_bxsize, vy0 + sht->m_bysize,
                              sht->m_height, sht->m_height);
     }
 }
 
+// 移动图层
 void win_sheet_updown(win_sheet_t *sht, int height) {
     int old = sht->m_height;
     win_sheet_ctl_t *ctl = g_sheet_ctl;
@@ -165,10 +178,14 @@ void win_sheet_updown(win_sheet_t *sht, int height) {
 
             ctl->m_sheets[height] = sht;
 
+            // 重新计算[height+1, top]图层到map的映射
+            // height层在向下移动的前后管理的像素只会减少，不会增多.
+            // 所以从`height-1`开始
             win_sheet_refreshmap(sht->m_vx0, sht->m_vy0,
                                  sht->m_vx0 + sht->m_bxsize,
                                  sht->m_vy0 + sht->m_bysize, height + 1);
 
+            // 因为是图层间的向下移动，所以只需刷新[height+1, old]之间的图层
             win_sheet_refreshsub(sht->m_vx0, sht->m_vy0,
                                  sht->m_vx0 + sht->m_bxsize,
                                  sht->m_vy0 + sht->m_bysize, height + 1, old);
@@ -183,10 +200,12 @@ void win_sheet_updown(win_sheet_t *sht, int height) {
 
             ctl->m_top--;
 
+            // 重新计算所有图层到map的映射
             win_sheet_refreshmap(sht->m_vx0, sht->m_vy0,
                                  sht->m_vx0 + sht->m_bxsize,
                                  sht->m_vy0 + sht->m_bysize, 0);
 
+            // 因为是隐藏图层，所以只需刷新隐藏图层以下的图层[0, old-1]
             win_sheet_refreshsub(
                 sht->m_vx0, sht->m_vy0, sht->m_vx0 + sht->m_bxsize,
                 sht->m_vy0 + sht->m_bysize, BOTTOM_WIN_SHEET_HEIGHT, old - 1);
@@ -217,14 +236,17 @@ void win_sheet_updown(win_sheet_t *sht, int height) {
             ctl->m_top++;
         }
 
+        // 重新计算[height, top]图层到map的映射
         win_sheet_refreshmap(sht->m_vx0, sht->m_vy0, sht->m_vx0 + sht->m_bxsize,
                              sht->m_vy0 + sht->m_bysize, height);
 
+        // 刷新当前图层
         win_sheet_refreshsub(sht->m_vx0, sht->m_vy0, sht->m_vx0 + sht->m_bxsize,
                              sht->m_vy0 + sht->m_bysize, height, height);
     }
 }
 
+// 更新各个图层像素管理映射表，表中保存每个像素归那个图层管理，图层更新时只更新自己管理的像素
 void win_sheet_refreshmap(int vx0, int vy0, int vx1, int vy1, int h0) {
     if (vx0 < 0) {
         vx0 = 0;
@@ -245,6 +267,8 @@ void win_sheet_refreshmap(int vx0, int vy0, int vx1, int vy1, int h0) {
     for (int h = h0; h <= g_sheet_ctl->m_top; h++) {
         win_sheet_t *sht = g_sheet_ctl->m_sheets[h];
         unsigned char sid = sht - g_sheet_ctl->m_sheets0;
+
+        // 计算局部坐标系，相对于绘制的窗口的偏移
         int bx0 = vx0 - sht->m_vx0, by0 = vy0 - sht->m_vy0;
         int bx1 = vx1 - sht->m_vx0, by1 = vy1 - sht->m_vy0;
 
@@ -265,9 +289,12 @@ void win_sheet_refreshmap(int vx0, int vy0, int vx1, int vy1, int h0) {
         }
 
         for (int by = by0; by < by1; by++) {
-            int vy = sht->m_vy0 + by;
+            int vy = sht->m_vy0 + by; // 转换为全局坐标系
+
             for (int bx = bx0; bx < bx1; bx++) {
-                int vx = sht->m_vx0 + bx;
+                int vx = sht->m_vx0 + bx; // 转换为全局坐标系
+
+                // 不是透明像素，则归我管
                 if (sht->m_buf[by * sht->m_bxsize + bx] != sht->m_col_inv) {
                     g_sheet_ctl->m_map[vy * g_boot_info.m_screen_x + vx] = sid;
                 }
